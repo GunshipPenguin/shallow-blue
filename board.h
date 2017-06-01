dbcfb6464168a7fd688465fa2c05f59e69755756
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-06-01 13:18:09 -0700

    Add Rank and File constants

--- board.h
+++ board.h
@@ -38,9 +38,30 @@ class Board {
 
     U64 OCCUPIED;
 
+    static const U64 RANK_1 = 255ull;
+    static const U64 RANK_2 = 65280ull;
+    static const U64 RANK_3 = 16711680ull;
+    static const U64 RANK_4 = 4278190080ull;
+    static const U64 RANK_5 = 1095216660480ull;
+    static const U64 RANK_6 = 280375465082880ull;
+    static const U64 RANK_7 = 71776119061217280ull;
+    static const U64 RANK_8 = 18374686479671623680ull;
+
+    static const U64 FILE_1 = 9259542123273814144ull;
+    static const U64 FILE_2 = 4629771061636907072ull;
+    static const U64 FILE_3 = 2314885530818453536ull;
+    static const U64 FILE_4 = 1157442765409226768ull;
+    static const U64 FILE_5 = 578721382704613384ull;
+    static const U64 FILE_6 = 289360691352306692ull;
+    static const U64 FILE_7 = 144680345676153346ull;
+    static const U64 FILE_8 = 72340172838076673ull;
+
+
     U64 getOccupied();
     U64 getBlackPieces();
     U64 getWhitePieces();
+
+    U64 getPawnMoves();
 };
 
 #endif
