d0f1df5c2f22712bf21bed7e64db24f4098f9f8a
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-06-10 15:52:59 -0700

    Allow showbb.py to accept input from stdin

--- scripts/showbb.py
+++ scripts/showbb.py
@@ -3,23 +3,27 @@ Pretty prints a bitboard to the console given its hex or decimal value.
 '''
 import sys, textwrap
 
+def print_bb(bb_str):
+    # Handle hex/decimal bitboards
+    if bb_str.startswith('0x'):
+        bb = bin(int(bb_str, 16))
+    else:
+        bb = bin(int(bb_str, 10))
+
+    # Slice off the '0b'
+    bb = bb[2:]
+
+    bb = bb.replace('0', '.')
+    bb = bb.replace('1', '#')
+
+    if len(bb) < 64:
+        bb = '.' * (64-len(bb)) + bb
+
+    print bb_str
+    print '\n'.join([' '.join(line[::-1]) for line in textwrap.wrap(bb, 8)]) + '\n'
+
 if len(sys.argv) == 1:
-    print 'Syntax: showbb.py <bitboard>'
-    sys.exit(1)
-
-# Handle hex/decimal bitboards
-if sys.argv[1].startswith('0x'):
-    bb = bin(int(sys.argv[1], 16))
+    for line in sys.stdin.readlines():
+        print_bb(line.strip())
 else:
-    bb = bin(int(sys.argv[1], 10))
-
-# Slice off the '0b'
-bb = bb[2:]
-
-bb = bb.replace('0', '.')
-bb = bb.replace('1', '#')
-
-if len(bb) < 64:
-    bb = '.' * (64-len(bb)) + bb
-
-print '\n'.join([' '.join(line[::-1]) for line in textwrap.wrap(bb, 8)]) + '\n'
+    print_bb(sys.argv[1])
