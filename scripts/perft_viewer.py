e51566a1b714ae6575faf7a73567bd1b39d3e33d
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-06-15 16:16:51 -0700

    Add perft viewer script

---
+++ scripts/perft_viewer.py
@@ -0,0 +1,18 @@
+import sys
+
+if len(sys.argv) < 3:
+    print "Usage: python perft_viewer.py <correct perft file> <incorrect perft file>"
+    sys.exit(1)
+
+correctFile = open(sys.argv[1], 'r')
+incorrectFile = open(sys.argv[2], 'r')
+
+correctMap = {result.split(": ")[0]:int(result.split(": ")[1]) for result in correctFile.readlines()}
+incorrectMap = {result.split(": ")[0]:int(result.split(": ")[1]) for result in incorrectFile.readlines()}
+
+for move in correctMap:
+    try:
+        if correctMap[move] != incorrectMap[move]:
+            print "INCORRECT: ", move
+    except KeyError:
+        print "MOVE NOT FOUND: ", move
