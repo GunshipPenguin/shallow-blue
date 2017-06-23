61018250d5844d0a8ef66b1cc991700101575160
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-06-23 12:22:38 -0700

    Use const references in TranspTable, mark getters as const

--- src/transptable.cc
+++ src/transptable.cc
@@ -9,22 +9,22 @@ void TranspTable::clear() {
   _table.clear();
 }
 
-bool TranspTable::contains(ZKey key) {
+bool TranspTable::contains(const ZKey& key) const {
   std::unordered_map<U64, std::tuple<int, int, TranspTable::Flag> >::const_iterator got = _table.find(key.getValue());
   return got != _table.end();
 }
 
-int TranspTable::getScore(ZKey key) {
+int TranspTable::getScore(const ZKey& key) const {
   std::tuple<int, int, Flag> entry = _table.at(key.getValue());
   return std::get<0>(entry);
 }
 
-int TranspTable::getDepth(ZKey key) {
+int TranspTable::getDepth(const ZKey& key) const {
   std::tuple<int, int, Flag> entry = _table.at(key.getValue());
   return std::get<1>(entry);
 }
 
-TranspTable::Flag TranspTable::getFlag(ZKey key) {
+TranspTable::Flag TranspTable::getFlag(const ZKey& key) const {
   std::tuple<int, int, Flag> entry = _table.at(key.getValue());
   return std::get<2>(entry);
 }
