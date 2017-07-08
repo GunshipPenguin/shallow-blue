b08158e62580fd57c8c34463271c15d63bd99707
GunshipPenguin <GunshipPenguin@users.noreply.github.com>
2017-07-08 15:48:31 -0700

    Document Eval class

--- src/eval.h
+++ src/eval.h
@@ -4,27 +4,57 @@
 #include "board.h"
 #include "defs.h"
 
+/**
+ * @brief Represents the evaluation of a chess board at a given position for a given color.
+ */
 class Eval {
 public:
+
+  /**
+   * @brief Evaluate the given board for the given color.
+   */
   Eval(const Board&, Color);
+
+  /**
+   * @brief Get a score representing the favourability of the position for the color with which
+   * this Eval was constructed.
+   *
+   * @return A favourability score for the board and color.
+   */
   int getScore();
 
-  int _calcMobility(const Board&, Color);
-
 private:
-  // Piece values in centipawns
+  /**
+   * @name Piece material value constants.
+   *
+   * @{
+   */
   static const int PAWN_VALUE = 100;
   static const int KNIGHT_VALUE = 320;
   static const int BISHOP_VALUE = 330;
   static const int ROOK_VALUE = 500;
   static const int QUEEN_VALUE = 900;
+  /**@}*/
 
-  // Other weights
+  /**
+   * @brief Mobility weight factored into an evaluation score.
+   */
   static const int MOBILITY_WEIGHT = 1;
 
   int _score;
+  /**
+   * @brief Evaluate the given board for the given color and update this Evaluation's state appropriately
+   */
   void _doEval(const Board&, Color);
 
+  /**
+   * @brief Returns an estimate for the number of legal moves for the given color on the given board.
+   *
+   * @param Board The board to estimate the number of moves for
+   * @param Color The color to estimate the number of moves for
+   * @return An estimate for the number of moves for the given color on the given board.
+   */
+  int _calcMobility(const Board&, Color);
 };
 
 #endif
