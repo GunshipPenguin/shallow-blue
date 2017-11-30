b41b16cc108cf778b996168772e8ee94a774748b
GunshipPenguin <me@rhysre.net>
2017-11-30 13:43:55 -0500

    Document GeneralMovePicker class

--- src/generalmovepicker.h
+++ src/generalmovepicker.h
@@ -5,19 +5,55 @@
 #include "orderinginfo.h"
 #include "board.h"
 
+/**
+ * @brief MovePicker that returns moves in an optimal order for negamax search
+ */
 class GeneralMovePicker : MovePicker {
 public:
-  GeneralMovePicker(const OrderingInfo* _orderingInfo, const Board*, MoveList*);
+  /**
+   * @brief Constructs a new GeneralMovePicker for the given OrderingInfo, Board and MoveList.
+   *  
+   * @param orderingInfo OrderingInfo object containing information about the current state of the search
+   * @param board Current board state for all moves in the provided MoveList
+   * @param moveList Pointer to the MoveList to pick moves from
+   */
+  GeneralMovePicker(const OrderingInfo*, const Board*, MoveList*);
 
   bool hasNext() const override;
+
+  /**
+   * @brief Returns the next best move in this GeneralMovePicker's MoveList for negamax search.
+   * 
+   * Note that internally, this method performs a selection sort for one unpicked move of the
+   * highest value in this GeneralMovePicker's internal MoveList and thus has time complexity
+   * O(n) with respect to the size of the MoveList. Provided move ordering is close to optimal though,
+   * this should be the optimal behaviour as negamax search should hit a beta cutoff after a small
+   * number of good moves.
+   * 
+   * @return the next best move in this GeneralMovePicker's MoveList for negamax search.
+   */
   Move getNext() override;
 
 private:
+  /**
+   * @brief Assigns a value to each move in this GeneralMovePicker's MoveList representing desirability
+   * in a negamax search.
+   */
   void _scoreMoves();
 
+  /**
+   * @brief Position of the first unpicked move in this GeneralMovePicker's MoveList
+   */
   size_t _currHead;
 
+  /**
+   * @brief OrderingInfo object containing search related information used by this GeneralMovePicker
+   */
   const OrderingInfo* _orderingInfo;
+
+  /**
+   * @brief Board for all moves in the provided MoveList
+   */
   const Board* _board;
 };
 
