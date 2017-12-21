9d7d873fdc3d65d66b888fc372ccf8fd89e45149
GunshipPenguin <me@rhysre.net>
2017-12-20 20:18:41 -0500

    Refactor search tests to use .iterDeep() add extra test

--- test/search_test.cc
+++ test/search_test.cc
@@ -3,54 +3,65 @@
 #include "raytable.h"
 #include "attacktable.h"
 #include "board.h"
+#include "movepicker.h"
 
-TEST_CASE("Search works as expected") {
+TEST_CASE("Search works as expected", "[search]") {
   PSquareTable::init();
   ZKey::init();
   RayTable::init();
   AttackTable::init();
+  MovePicker::init();
 
-  SECTION("Search works as expected after PSquareTable and ZKey are initialized") {
-    Board board;
-    
-    SECTION("Search finds a checkmate on next move") {
-      // Fool's mate
-      board.setToFen("rnbqkbnr/pppp1ppp/4p3/8/6P1/5P2/PPPPP2P/RNBQKBNR b KQkq -");
+  Board board;
+  Search::Limits limits;
+  limits.time[WHITE] = 300000; 
+  limits.time[BLACK] = 300000;
+  limits.increment[WHITE] = 5000;
+  limits.increment[BLACK] = 5000;
+  limits.movesToGo = 50;
 
-      Search search(board, false);
-      search.perform(1);
+  SECTION("Search finds the fool's mate checkmakte on the next move") {
+    board.setToFen("rnbqkbnr/pppp1ppp/4p3/8/6P1/5P2/PPPPP2P/RNBQKBNR b KQkq -");
 
-      REQUIRE(search.getBestMove().getNotation() == "d8h4");
-    }
+    Search search(board, limits, false);
+    search.iterDeep();
 
-    SECTION("Search returns the only legal move when checkmate is 1 move away") {
-      board.setToFen("r4rk1/ppp2ppp/4p3/8/4p3/4PPbP/PPPB2q1/R2QKR2 w - -");
+    REQUIRE(search.getBestMove().getNotation() == "d8h4");
+  }
 
-      Search search(board, false);
-      search.perform(2);
+  SECTION("Search returns the only legal move when checkmate is 1 move away") {
+    board.setToFen("r4rk1/ppp2ppp/4p3/8/4p3/4PPbP/PPPB2q1/R2QKR2 w - -");
 
-      REQUIRE(search.getBestMove().getNotation() == "f1f2");
-    }
+    Search search(board, limits, false);
+    search.iterDeep();
 
-    SECTION("Search recognizes when a check can be made to capture a queen") {
-      board.setToFen("8/4N3/8/1k5q/8/8/8/2K2R2 w - -");
+    REQUIRE(search.getBestMove().getNotation() == "f1f2");
+  }
 
-      Search search(board, false);
-      search.perform(3);
+  SECTION("Search recognizes when a check can be made to capture a queen") {
+    board.setToFen("8/4N3/8/1k5q/8/8/8/2K2R2 w - -");
 
-      REQUIRE(search.getBestMove().getNotation() == "f1f5");
-    }
+    Search search(board, limits, false);
+    search.iterDeep();
 
-    SECTION("Search finds a checkmate on the next move with multiple searches with depth > 1") {
-      board.setToFen("2kr3r/pp4pp/4N3/q7/2K5/8/PR1b2PP/8 b - - 7 33");
+    REQUIRE(search.getBestMove().getNotation() == "f1f5");
+  }
 
-      Search search(board, false);
+  SECTION("Search finds a checkmate on the next move") {
+    board.setToFen("2kr3r/pp4pp/4N3/q7/2K5/8/PR1b2PP/8 b - - 7 33");
 
-      search.perform(1);
-      REQUIRE(search.getBestMove().getNotation() == "a5d5");
+    Search search(board, limits, false);
+    search.iterDeep();
 
-      search.perform(2);
-      REQUIRE(search.getBestMove().getNotation() == "a5d5");
-    }
+    REQUIRE(search.getBestMove().getNotation() == "a5d5");
+  }
+
+  SECTION("Bratko-Kopec test #1 is correct") {
+    board.setToFen("1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - -");
+
+    Search search(board, limits, false);
+    search.iterDeep();
+
+    REQUIRE(search.getBestMove().getNotation() == "d6d1");
   }
 }
