938c8f9631a47730f51053e09c90b564e7f7dc50
Rhys Rustad-Elliott <me@rhysre.net>
2019-01-16 10:54:20 +0000

    Update Windows build script
    - Add -fno-exceptions to CC_FLAGS
    - Add shebang

--- build_windows.sh
+++ build_windows.sh
@@ -1,21 +1,22 @@
+#!/usr/bin/env bash
 # Cross compile script to build windows binaries
 # Depends on Mingw-w64
 
-export CC_FLAGS="-Wall -std=c++11 -O3 -flto -pthread -mtune=generic -static"
+export CC_FLAGS="-Wall -std=c++11 -O3 -flto -pthread -mtune=generic -fno-exceptions -static"
 export LD_FLAGS="-pthread -flto -static"
 export CXX=x86_64-w64-mingw32-g++-posix
 make clean
-make
+make -j4
 mv shallowblue shallowblue_x86-64.exe
 
 export CC_FLAGS="-Wall -std=c++11 -O3 -flto -pthread -mtune=generic -static -m32"
 export LD_FLAGS="-pthread -flto -static -m32"
 export CXX=i686-w64-mingw32-g++-posix
 make clean
-make
+make -j4
 mv shallowblue shallowblue_x86.exe
 
-echo "\n==========="
+echo -e "\n==========="
 echo "Windows builds created"
 echo "SHA256 sums"
 sha256sum shallowblue_x86.exe shallowblue_x86-64.exe
