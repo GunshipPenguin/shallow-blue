92849bfef5ea47797ea93fe230a282ee12df6c01
Oscar <oggyp@oggyp.com>
2022-09-07 16:26:02 +1000

    fixing hui

--- build_windows.sh
+++ build_windows.sh
@@ -4,14 +4,14 @@
 
 export CC_FLAGS="-Wall -std=c++11 -O3 -flto -pthread -mtune=generic -fno-exceptions -static"
 export LD_FLAGS="-pthread -flto -static"
-export CXX=i686-w64-mingw32-g++
+export CXX=i686-w64-mingw32-g++-posix
 make clean
 make -j4
 mv shallowblue shallowblue_x86-64.exe
 
 export CC_FLAGS="-Wall -std=c++11 -O3 -flto -pthread -mtune=generic -static -m32"
 export LD_FLAGS="-pthread -flto -static -m32"
-export CXX=i686-w64-mingw32-g++
+export CXX=i686-w64-mingw32-g++-posix
 make clean
 make -j4
 mv shallowblue shallowblue_x86.exe
--- src/uci.h
+++ src/uci.h
@@ -9,7 +9,6 @@
 #include <sstream>
 #include <fstream>
 #include <memory>
-#include <thread>
 
 
 /**
